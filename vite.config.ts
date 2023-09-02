8f53fd2b19dbfad7fc55a708157e96dd21d73058
yurygrivickiy <yury.grivickiy@finespace.com>
2023-09-02 21:11:19 +0300

    fix: upd base url in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: './git@github.com:AD1N1993/aw-test.git',
+    plugins: [react()],
 })
