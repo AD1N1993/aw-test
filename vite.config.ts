66eece00218618c8f9c570ee80ed4c3a34acdef9
yurygrivickiy <yury.grivickiy@finespace.com>
2023-09-02 21:25:17 +0300

    fix: add home page to package.json - upd base address

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './git@github.com:AD1N1993/aw-test.git',
+    base: '/aw-test/',
     plugins: [react()],
 })
